115ac61a8e57df5de6864abd3524ecb96e19367a
rseoaneig <20g1rseoane@gmail.com>
2025-05-21 00:04:20 +0200

    Continuacion de funciones para los presupuestos

--- scripts.js
+++ scripts.js
@@ -8,7 +8,10 @@ window.onload = function () {
     document.getElementById("tejado").classList.add("oculto");
     document.getElementById("fontaneria").classList.add("oculto");
 
-    document.getElementById("bCalcPresupuesto").addEventListener("click", calcularBano);
+    document.getElementById("bCalcularBano").addEventListener("click", calcularBano);
+    document.getElementById("bCalcularCocina").addEventListener("click", calcularCocina);
+    document.getElementById("bCalcularTejado").addEventListener("click", calcularTejado);
+    document.getElementById("bCalcularFont").addEventListener("click", calcularFont);
 }
 
 function cambiarPresupuestoBano() {
@@ -59,7 +62,47 @@ function calcularBano() {
     } else if (m2azulejo == "" && azulejo.checked) {
         result.innerHTML = "Por favor, seleccione una cantidad de metros cuadrados de azulejo";
     } else if (isNaN(m2azulejo)){
-        result.innerHTML = "Por favor, introduce un numero en la cantidad de metros cuadrados de azulejo";
+        result.innerHTML = "Por favor, introduce un número en la cantidad de metros cuadrados de azulejo";
+    } else {
+        result.innerHTML = "El presupuesto total es " + total + "€";
+    }
+}
+
+function calcularCocina(){
+    let total = 0;
+    let result = document.getElementById("resultadoCocina");
+
+    const fregadero = document.getElementById("fregadero");
+    const cExtractora = document.getElementById("cExtractora");
+    const encimera = document.getElementById("encimera");
+    const matEncimera = document.querySelector('input[name="matEncimera"]:checked').value;
+    const m2encimera = document.getElementById("m2encimera").value;
+    let valorMaterial;
+
+    if(matEncimera == "granito"){
+        valorMaterial = 200;
+    } else if (matEncimera == "marmol") {
+        valorMaterial = 230;
+    } else if (matEncimera == "basalto") {
+        valorMaterial = 300;
+    }
+
+    if(fregadero.checked){
+        total += 300;
+    }
+    if(cExtractora.checked){
+        total += 200
+    }
+    if(encimera.checked){
+        total += m2encimera * valorMaterial;
+    }
+
+    if (m2encimera != "" && !encimera.checked) {
+        result.innerHTML = "Por favor, seleccione el check de encimera para elegir los metros cuadrados";
+    } else if (m2encimera == "" && encimera.checked) {
+        result.innerHTML = "Por favor, seleccione una cantidad de metros cuadrados de encimera";
+    } else if (isNaN(m2encimera)){
+        result.innerHTML = "Por favor, introduce un número en la cantidad de metros cuadrados de encimera";
     } else {
         result.innerHTML = "El presupuesto total es " + total + "€";
     }
